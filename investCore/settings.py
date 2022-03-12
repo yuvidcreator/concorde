8623684c2e2e6b43f95a84c60f02344530a79106
unknown <yuvrajl.103@gmail.com>
2022-03-13 00:46:33 +0530

    db change

--- investCore/settings.py
+++ investCore/settings.py
@@ -98,7 +98,7 @@ if DEBUG:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.environ.get('DB_NAME'),
             'USER': os.environ.get('DB_USER'),
             'PASSWORD': os.environ.get('DB_PASS'),
