94761eba47c40d24aed3ee4c30f377599d65140b
unknown <yuvrajl.103@gmail.com>
2022-03-13 01:06:58 +0530

    static folder

--- investCore/settings.py
+++ investCore/settings.py
@@ -169,7 +169,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'assets/images')
 
-STATICFILES_DIRS = [ BASE_DIR / 'assets' ]
+STATICFILES_DIRS = [ BASE_DIR / 'assets', '/var/www/assets', ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
